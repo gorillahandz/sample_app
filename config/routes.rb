1d5eb1a3f209f53060a865b59edf2d338a7fb307
Rohan Prabhakar <rohan.prabhakar@flexport.com>
2020-03-12 12:22:24 -0700

    Some updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #setting the root page to be the OM action
-  root 'application#om'
   
+  get 'static_pages/home'
+  get 'static_pages/help'
+
+  root 'application#om'
+
 end
